60d14e60df6537b1166f3d639dc27964f9364867
shreetuition <info@shreetuitioncenter.com>
2021-12-06 15:20:25 +0530

    Done

---
+++ JS/custom.js
@@ -0,0 +1,197 @@
+//alert("Hello")
+
+// console.log("Hello")
+// document.write("Hello Kumar")
+
+// Rules Of Js 
+
+// A variable starts with the var  sign, followed by the name of the variable
+// A variable name must start with a letter or the underscore character
+// A variable name cannot start with a number
+// A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+// Variable names are case-sensitive (age and AGE are two different variables)
+
+// var a = 4 // Number
+
+// var a = 5.6565
+
+// var username = "Parth Bua" // string
+
+// var isStatus = true // String
+
+// // typeof()
+
+// var age = 10 
+
+// var Age = 15 
+
+// console.log(age);
+
+// 1. camcale case 
+// 2. snake case 
+// 3. pascal case 
+
+// var userName = "path"
+// var user_name = "path"
+// var UserName = "Path"
+
+// var ageOfPerson // camcale case
+// var age_of_person  // snake case 
+
+// var AgeOfPerson  // pascal case 
+
+/*console.log(typeof(a));
+console.log(typeof(username));
+console.log(typeof(isStatus));
+console.log(typeof(a));
+
+console.log(isStatus);
+
+console.log(username);
+
+console.log(a);
+
+console.log(a);
+console.log(a);
+console.log(a);
+console.log(a);
+console.log(a);*/
+
+'use strict'
+
+// var a = 10 
+
+// console.log(a);
+
+// {
+// 	var a = 45
+
+// 	console.log(a);
+// }
+
+// console.log(a);
+
+// let a = 25
+
+//console.log(a); 
+
+// {
+// 	let a = 88
+
+// 	console.log(a);
+// }
+
+// console.log(a);
+
+
+// const pi = 7.14
+
+// pi = 4
+
+// console.log(pi)
+
+// document.write("<h1>Hello Kumar</h1>")
+
+// Maths Operator
+
+// + 
+// - 
+// * 
+// / 
+// % 
+// =
+// == 
+// ===
+// +=
+// -=
+// *= 
+// ++
+// -- 
+// != 
+// > 
+// < 
+// >= 
+// <= 
+// && 
+// || 
+
+// let a = 55
+// let b = 15
+
+// console.log(a+b); // 70
+// console.log(a-b); // 40
+// console.log(a*b); // 825
+// console.log(a/b); // 3.66
+// console.log(a%b); // 10
+// console.log(a=b); // 
+
+// console.log(a==b); // false
+
+// // a = 15 
+
+// // b = 55 
+
+// console.log(a);
+
+// console.log(b);
+
+// console.log(a===b); // true
+
+// console.log(a!=b); // false
+
+// console.log(a>b); // false
+// console.log(a<b); // false
+// console.log(a>=b); // true
+// console.log(a<=b); // false
+
+
+/*
+let a = 55
+let b = 15
+
+console.log(a); // 55 // 55 // 55 // 55 / 55
+
+console.log(a++); // 56 // 56 // 56 // 56 // 55
+
+console.log(a); // 56 // 55 // 56 // 56 // 56
+
+console.log(++a); // 57 // 57 // 58 // 57 // 57
+
+console.log(--a); // 56 // 56 //56 /
+console.log(++a); //57
+console.log(a++); // 57
+console.log(a); // 58
+
+console.log(--a); // 57
+console.log(a); // 57 // 58 // 56 // 57 // 57
+console.log(a--); // 57 // 57
+console.log(a);*/
+
+
+// console.log(a>=b);
+
+
+
+
+/*let a = 10
+
+console.log(a) // 10
+
+a += 1 // a = a + 1
+
+console.log(a) // 11 // 10 // 10 // 10 // 11*/
+
+
+// let a = 10 
+
+// let b = 22
+
+// console.log(a>20 && a<25); // false // false // false
+
+// console.log(a>20 || a<25); // true // true
+
+
+
+
+
+
