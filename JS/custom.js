8c5e3b66046e15fc1f9cd25f863c64e9a2fea6b6
shreetuition <info@shreetuitioncenter.com>
2021-12-08 15:10:07 +0530

    Done

--- JS/custom.js
+++ JS/custom.js
@@ -170,9 +170,6 @@ console.log(a);*/
 
 // console.log(a>=b);
 
-
-
-
 /*let a = 10
 
 console.log(a) // 10
@@ -191,7 +188,136 @@ console.log(a) // 11 // 10 // 10 // 10 // 11*/
 // console.log(a>20 || a<25); // true // true
 
 
+// if (true) {}
+
+// if (true) {} else {}
+
+// if (true) {} else if {} else {}
+
+// switch() {
+
+// }
+
+// if(false) {
+// 	console.log('Hello');
+// }
+
+/*let a = 100;
+
+if (a>=15) {
+	console.log(a);
+}
+*/
 
 
+let a = 10 
+
+// let b = 22
+
+// if (a>20 || a<25 || b>10) {
+// 	console.log(a);
+// }
 
 
+// if(a<=10) {
+// 	console.log(a);
+// } else {
+// 	console.log('Error');
+// }
+
+// 1 - 11:59 => M 
+// 12 - 16 // E 
+//17 - 23 - GN  
+
+// let time = 20;
+
+// if(time>=1 && time<12) {
+// 	console.log('GM');
+// } else if(time>=12 && time<16) {
+// 	console.log('GA');
+// } else {
+// 	console.log('GN');
+// }
+
+
+let x = 0;
+
+// if(x>=0) {
+// 	if(x==0) {
+// 		console.log(x, " is Neutral Number");
+// 	} else {
+// 		console.log(x, " is +ve");
+// 	}
+	
+// } else {
+// 	console.log(x, " is -ve");
+// }
+
+// console.log(screen.width);
+
+// if(x>0) {
+// 	console.log(x, " is +ve");	
+// } else if(x==0) {
+// 	console.log(x, " is Neutral Number");
+// } else {
+// 	console.log(x, " is -ve");
+// }
+
+// if(screen.width>=1200 && screen.width<1800) {
+// 	document.write("Hello Kumar")
+// } else if(screen.width>=500 && screen.width<1200) {
+// 	document.write("Hello Sharvan")
+// }
+
+// let p = 15987;
+// let roi = 15;
+// let t = 2;
+// let si = 0;
+// let totalA = 0;
+// si = (p*roi*t)/100;
+// totalA = p + si;
+
+// console.log("P = ", p);
+
+// console.log("SI = ", si);
+
+// console.log('Total = ', totalA);
+
+// let number = 4500;
+
+// if(number%2==0) {
+// 	console.log(number , ' is Even');
+// } else {
+// 	console.log(number , ' is Odd');
+// }
+
+/*2000, 2004, 2008, 2012, 2016, 2020, 2024, 2028, 2032, 2036, 2040, 2044, and 2048*/
+
+/*
+let year = 2036;
+
+if(year%4==0) {
+	console.log(year, ' Leap Year');
+} else {
+	console.log(year, ' Not Leap Year');
+}*/
+
+// (0°C × 9/5) + 32 = 32°F (C to F)
+
+// (0°F − 32) × 5/9 = -17.78°C  (C to F)
+
+let c=15;
+let result = ((c * 9/5) + 32);
+console.log(result);
+
+let f=20;
+let degree = ((f - 32) * (5/9));
+	console.log(degree);
+
+// JavaScript Program to Convert Kilometers to Miles
+
+// JavaScript Program to Swap Two Variables
+
+// JavaScript Program to Calculate the Area of a Triangle
+
+// JavaScript Program to Swap Two Variables without using third variables
