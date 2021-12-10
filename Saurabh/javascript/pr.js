a6a82037fee88589d96b77b7b15b9f588434b69f
Saurabh Bhuva <saurabhbhuva53@gmail.com>
2021-12-10 10:42:29 +0530

    Done

---
+++ Saurabh/javascript/pr.js
@@ -0,0 +1,40 @@
+// JavaScript Program to Convert Kilometers to Miles
+
+// let km = 15;
+
+// let mi = km/1.609344;
+
+// console.log(mi);
+
+
+// JavaScript Program to Swap Two Variable
+
+// var a = 10;
+// var b = 20;
+
+// console.log("Before swapping the value of a is " + a + " and b is " + b);
+
+// var c = a;
+// a = b;
+// b = c;
+// console.log("After swapping the value of a is " + a + " and b is " + b);
+
+// JavaScript Program to Calculate the Area of a Triangle
+
+// let h = 10;
+// let b = 20;
+
+// let a = (h * b)/2;
+
+// console.log(a);
+
+// JavaScript Program to Swap Two Variables without using third variables
+
+// var a = 10;
+// var b = 20;
+
+// console.log("Before swapping the value of a is " + a + " and b is " + b);
+// a = a + b; //30
+// b = a - b; //10
+// a = a - b; //20
+// console.log("After swapping the value of a is " + a + " and b is " + b);
