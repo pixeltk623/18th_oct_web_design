fb2936d546fefe7637c376d911804318e2edbdf7
patel08 <phpatel700@gmail.com>
2021-12-10 14:09:15 +0530

    done

---
+++ Parth/js/customs1.js
@@ -0,0 +1,57 @@
+ // let d=1;
+ // let b=2;
+ //function swap(){//something}
+ //swap(d,b);
+ //console.log(d) //2
+ //console.log(b) //1
+
+
+let k=3
+let output = k * 0.621371  //1 km =  0.621371 miles 
+console.log(output);
+
+let km=4.5
+let outin = km * 0.621371;
+console.log(outin);
+
+
+let m = prompt('enter the first variable')
+let n = prompt('enter the second variable')
+
+let o;
+
+o = m;
+m = n;
+n = o;
+
+console.log(`value of m after swapping: ${m}`);
+console.log(`value of n after swapping: ${n}`);
+
+let basevalue = prompt('enter a base of triangle')
+let highvalue = prompt('enter a high of triangle')
+
+let areavalue = basevalue * highvalue / 2;
+
+console.log(`The area of the triangle is ${areavalue}`);
+
+
+//let h = "Hello", w = "World!";
+//console.log(h, w); // Hello World!
+//[h, w] = [w, h]; //
+//console.log(w, h); // World! Hello
+
+
+let s = prompt('Enter first number')
+let y = prompt('Enter second number')
+
+s = s + y
+y = s - y
+s = s - y
+
+console.log(`After swap s is: ${+ s}`)
+console.log(`After swap y is: ${+ y}`)
+
+
+
+
+ 
