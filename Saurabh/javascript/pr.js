65cf367740eb0d151a39af5d4a6e8bd2622f9ca3
Saurabh Bhuva <saurabhbhuva53@gmail.com>
2021-12-14 17:12:22 +0530

    Done

--- Saurabh/javascript/pr.js
+++ Saurabh/javascript/pr.js
@@ -67,11 +67,11 @@
 // 		y = 1;
 // 		break;
 // 	}
-// }
 // if (i > 1 && y == 0) {
 // 	console.log(i);
 // }
-// }
+
+
 // armstrong
 
 // var num = prompt("Enter the number");
@@ -85,6 +85,8 @@
 //  }else{
 //  	console.log(num + " is not an armstrong number");
 //  }
+
+
 // palindrome
 
 // var string = prompt("Enter a string");
@@ -110,3 +112,168 @@
 // 	sum +=i;
 // }
 // console.log(sum);
+
+// ax^2+bx+c=0;
+
+// Quadratic Equation
+
+// var a = 1;
+// var b = 6;
+// var c = 5;
+// var d = b * b - 4 * a * c ;
+// var r1,r2;
+
+// if(d>0){
+// 	r1 = (-b + Math.sqrt(d)) / (2 * a);
+// 	r2 = (-b - Math.sqrt(d)) / (2 * a);
+
+// 	console.log("The Quadratic Equation are " + r1  + " and " + r2);
+// }
+
+// randam value
+// var a = Math.random();
+// console.log(a);
+
+// Positive, Negative, or Zero
+
+// var a = 0;
+
+// if(a>0){
+// 	console.log("The value is Positive");
+// }else if(a<0){
+// 	console.log("The value is Negative");
+// }else{
+// 	console.log("The value is Zero");
+// }
+
+// largest among three number
+
+// var a = 10;
+// var b = 20;
+// var c = 30;
+
+
+// if(a > b  &&  a > c){
+
+// 	console.log("The largest numbar is " +  a);
+
+// }else if(b> a  &&  b > c){
+
+// 	console.log("The largest numbar is " +  b);
+
+// }else if(c > a  &&  c > b){
+
+// 	console.log("The largest numbar is " +  c);
+
+// }
+
+// Print All Prime Numbers in an Interval
+
+// (1,10)
+
+// a = 1;
+// b =10;
+
+// console.log("The prime numbar are :");
+		
+// for (var i = a; i <= b; i++) {
+// 	var y = 0;
+// 	for (var j = 2; j<i; j++) {
+// 		if (i%j==0) {
+// 		y = 1;
+// 		break;
+// 	}
+// }
+// if (i > 1 && y == 0) {
+// 	console.log(i);
+// }
+// }
+
+// Armstrong Number in an Interval
+
+// var a = prompt("Enter the first  number");
+// var b = prompt("Enter the second  number");
+
+// for(var i = a; i<=b; i++){
+// 	var numOfDigits = i.toString().length;
+// 	var sum = 0;
+// 	var temp = i;
+// 	while(temp > 0){
+// 		var digit = temp%10;
+// 		sum += digit ** numOfDigits;
+// 		temp = parseInt(temp/10);
+// 	}
+// 	if(sum == i){
+// 		console.log(i);
+// 	}
+// }
+
+
+
+
+// const lowNumber = parseInt(prompt('Enter a positive low integer value: '));
+// const highNumber = parseInt(prompt('Enter a positive high integer value: '));
+
+// console.log ('Armstrong Numbers:');
+
+// for (let i = lowNumber; i <= highNumber; i++) {
+
+
+//     let numberOfDigits = i.toString().length;
+
+//     let sum = 0;
+
+   
+//     let temp = i;
+
+//     while (temp > 0) {
+
+//         let remainder = temp % 10;
+//         sum += remainder ** numberOfDigits;
+      
+//         temp = parseInt(temp / 10); 
+//     }
+ 
+//     if (sum == i) {
+//         console.log(i);
+//     }
+// }
+
+// factorial number
+
+// var a = 5;
+// var fact = 1;
+
+// for (var i = 1; i<=a;i++) {
+	 
+// 	fact *=i;
+
+// }
+// console.log("The factorial of " + a +  " is " + fact );
+
+// multiplication table
+
+// var a =2;
+
+// for(i = 1; i <= 10; i++){
+// 	var r = a * i ;
+
+// 	console.log(a + " * " + i + " = " + r);
+// }
+
+ // fibonacci series
+
+
+var num = 10;
+var a = 0, b = 1, result;
+
+console.log("The fibonacci series is :");
+
+
+for(var i = 1; i <= num;i++){
+	console.log(a);
+	result = a + b;
+	a = b;
+	b = result;
+
+}
